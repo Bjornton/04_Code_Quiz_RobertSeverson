e644e51143fb37f4c7a2bbcf56a1de930cbf4762
Bjornton <bj.severson34@gmail.com>
2022-11-08 17:06:35 -0600

    Added questions to the script.js file

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+// Questions were gathered from https://www.w3schools.com/quiztest/quiztest.asp?qtest=JS
+var questions = [
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<script>", "<javascript>", "<scripting>", "<js>"],
+        answer: "<script>"
+    },
+
+    {
+        title: "How do you create a function in JavaScript?",
+        choices: ["function:myFunction()", "function myFunction()", "function = myFunction()", "function - myFunction()"],
+        answer: "function myFunction()"
+    },
+
+    {
+        title: "How can you add a comment in JavaScript?",
+        choices: ["//Comment", "'Comment", "!!!Comment", "<!--Comment-->"],
+        answer: "//Comment"
+    },
+
+    {
+        title: "How do you round the number 7.25 to the nearest integer?",
+        choices: ["round(7.25)", "Math.rnd(7.25)", "Math.round(7.25)", "rnd(7.25)"],
+        answer: "Math.round(7.25)"
+    },
+
+    {
+        title: "Which operator is used to assign a value to a variable?",
+        choices: ["-", "x", "=", "*"],
+        answer: "="
+    }
+];
